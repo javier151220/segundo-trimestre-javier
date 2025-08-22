934fb47f7a55f6fd093d93a9a47020b9098dc1f2
javier solis <jsruiz932@gmail.com>
2025-08-22 17:40:09 -0500

    SENA

---
+++ Bucles.py
@@ -0,0 +1,57 @@
+# # EJEMPLOS
+# print("------------------ejercico numeo 1---------------------------------------------------- ")
+
+# num=int(input("ingresa un numero : "))
+# while num> 0:
+#     print(f"El numero ingresado es {num}")
+#     num -=1
+
+# print("Se acabo el conteo") #sacr el print del bucle porque sino cada ves ma sale en cada nuemro 
+
+
+
+# print("------------------------ejercico numeo 2------------------------------------------------")
+
+
+
+# numero=int(input("Ingresa el numero de la tabla que vas a multiplicar  a multiplicar :")) #pedir al usuario el numero
+# i =1 #Inicia  primer multiplicador 
+
+# print(f"")
+# while i<= 10 : #bucle que repite  i sea manor o igual a 10
+#     print(f"{numero}*{i} = {numero * i}")#moestra la multiplicacion y su reultado
+#     i +=1 #actualizacion
+
+
+
+
+# print("----------------ejercicio numero 3--------------------------")
+
+# numero = 1 
+# while True:
+#     print(numero)
+#     numero += 1
+#     break
+
+# print("----------------ejercicio numero 4 --------------------------")
+
+# numero=int(input("ingresa un numero par :"))
+# while True:
+#     numero -=1
+#     print(numero)
+#     if numero==0:
+#         break
+# print("FIN DEL BUCLE ")
+
+
+# print("----------------ejercicio numero 5--------------------------")
+
+# chance =1
+# while chance <=3:
+#     txt=input("Escribe si :")
+#     if txt =="si":
+#         print("OK, lo conseguiste en el intento", chance)
+#         break
+#     chance += 1
+# else:
+#     print("Has agotado tus 3 intntos")
---
+++ Ejecicios_bucles.py
@@ -0,0 +1,32 @@
+print("EJERCIICIOS CO WHILE, CONDICIONALE Y ESTRUCTURAS ")
+
+
+
+# print("---------------------------------EJERCICIO 1 ---suma hasta cero -------------------------------------------- ")
+# num=0
+# Numero=int(input("ingresa un numero entero  :"))
+# while True:
+#     if Numero>0:
+#         num +=Numero
+#     else:
+#         break
+# print(f"se cerro el promgrama")
+    
+
+# print("------------------------jercicio -2--------------------------------")
+
+# contra=""
+# while contra !="python1215":
+#     contra=input("ingresa la contraseña : ")
+# print("contraseña correcta")
+
+
+# print("------------------------jercicio -3--------------------------------")
+
+# productos=[]
+# while True:
+#     pro=input("ingresa un producto :")
+#     if pro=="fin":
+#         break
+#     productos.append(pro)
+# print(f"En total pediste: {productos}")
